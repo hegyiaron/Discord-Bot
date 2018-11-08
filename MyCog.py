a731c400e136e7850a4debe2dde892a097c8f593
hegyiaron <44873487+hegyiaron@users.noreply.github.com>
2018-11-08 19:46:53 +0100

    Create MyCog.py

---
+++ MyCog.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from apiclient.discovery import build
+import json
+from datetime import datetime
+import time
+import asyncio
+import sys
+import logging
+import aiohttp
+import re
+
+bot = commands.Bot(command_prefix='-', description=None)
+
+#A "cog" létrehozása, amely a fő scriptben előlesz hívva
+class MyCog:
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
+    @commands.command(pass_context=True)
+    async def cog_command(self, ctx):
+        return 0
+
+def setup(bot):
+    bot.add_cog(MyCog(bot))
